8e2ccc10d3c1478152fbf2014535ad1ff01e5201
Trevor Starick <trevor.starick@gmail.com>
2014-06-22 20:00:00 -0400

    Random fixes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express');
+var hbs  = require('express-hbs');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
@@ -13,7 +14,8 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('hbs', hbs.express3());
+app.set('view engine', 'hbs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -22,7 +24,6 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,3 @@
 exports.index = function(req, res){
-  res.render('index');
+	res.render('index', {quote:'Do or do not; there is no try.'});
 };
